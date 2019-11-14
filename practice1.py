2d4a648b8e614e814f478fac30bb75495c12fc38
rubakhan9430 <rubakhan9430@gmail.com>
2019-11-14 16:47:08 -0600

    String manipulations

---
+++ practice1.py
@@ -0,0 +1,8 @@
+sentence1 = 'I "really" lke chocolate'
+sentence2 = "I 'really' like chocolate"
+
+sentence1 = 'I \'really\' like chocolate'
+sentence2 = "I \"really\" like chocolate'
+
+print(sentence1)
+print(sentence2)
